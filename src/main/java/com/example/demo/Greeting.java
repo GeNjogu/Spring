edfc4ac7775e5ca2ff573b0e0a0f57148798e3c0
Geoffrey Njogu <32560631+GeNjogu@users.noreply.github.com>
2024-03-27 09:28:39 +0200

    Update Greeting.java

--- src/main/java/com/example/demo/Greeting.java
+++ src/main/java/com/example/demo/Greeting.java
@@ -4,7 +4,7 @@ public class Greeting {
 
     private final long id;
     private final String content;
-    private final string username;
+    private final String username;
 
     public Greeting(long id, String content) {
         this.id = id;
